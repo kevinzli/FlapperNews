9b4c3a03e87fc0129b02a6a5065559879f265729
Kevin Li <li.zhi.jun@hotmail.com>
2015-11-10 23:59:48 -0800

    Implement post page with comments

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var app=angular.module('flapperNews',[]);
+var app=angular.module('flapperNews',['ui.router']);
+
+app.factory('posts', [function(){
+	var o={
+		posts:[]
+	};
+	return o;
+}])
 
 app.controller('MainCtrl', [
 	'$scope',
-	function($scope){
+	'posts',
+	function($scope,posts){
 		$scope.test='Hello world!';
 
-		$scope.posts=[
-		 	{title: 'post 1', upvotes: 5},
-			{title: 'post 2', upvotes: 2},
-			{title: 'post 3', upvotes: 15},
-			{title: 'post 4', upvotes: 9},
-			{title: 'post 5', upvotes: 4}
-		];
+		$scope.posts=posts.posts;
 
 		$scope.addPost=function(){
 			if(!$scope.title||$scope.title===''){
@@ -20,12 +22,58 @@ app.controller('MainCtrl', [
 			$scope.posts.push({
 				title:$scope.title, 
 				link:$scope.link,
-				upvotes:0});
+				upvotes:0,
+				comments: [
+    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  ]
+});
 			$scope.title='';
 			$scope.link='';
 		};
 
 		$scope.incrementUpvotes=function(post){
 			post.upvotes +=1;
+		}
+}]);
+
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('home',{
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl'
+			})
+			.state('posts',{
+				url: '/posts/{id}',
+				templateUrl: '/posts.html',
+				controller: 'PostsCtrl'
+			});
+
+		$urlRouterProvider.otherwise('home');
+}]);
+
+app.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	 function($scope, $stateParams, posts){
+	 	$scope.post=posts.posts[$stateParams.id];
+
+	 	$scope.addComment = function(){
+		  if($scope.body === '') { return; }
+		  $scope.post.comments.push({
+		    body: $scope.body,
+		    author: 'user',
+		    upvotes: 0
+		  });
+		  $scope.body = '';
+		};
+		
+		$scope.incrementUpvotes=function(comment){
+			comment.upvotes +=1;
 		}
 }]);
