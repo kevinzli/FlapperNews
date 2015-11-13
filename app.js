2154ec6d71c7b7ef444b95a41bd443fd95008a7e
Kevin Li <li.zhi.jun@hotmail.com>
2015-11-12 23:16:25 -0800

    Implement authentication

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
+var passport = require('passport');
 
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 
 mongoose.connect('mongodb://localhost/news');
 
@@ -29,6 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
